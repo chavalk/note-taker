007e84c0fd9fffd2e9034904d026088850ac54e7
chavalk <chavalk@hotmail.com>
2021-01-11 20:43:32 -0600

    Added path dependecy and routes for notes.html and index.html.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require("express");
 var fs = require("fs");
+var path = require("path");
 
 // Sets up Express app
 var app = express();
@@ -11,6 +12,15 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Routes
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 // Listener
 app.listen(PORT, function() {
